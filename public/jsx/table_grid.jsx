0d26171591f6316bfa10410e0b7c984e2289a886
Ralf Nieuwenhuijsen <r.nieuwenhuijsen@synovite.nl>
2015-04-25 23:43:47 +0200

    * fullscreen tables (for now) * text-as-you-type implemented

--- public/jsx/table_grid.jsx
+++ public/jsx/table_grid.jsx
@@ -1,10 +1,10 @@
 var TableGrid = React.createClass({	
-	getInitialState:function(){	
+	getInitialState:function(){			
 		return { fields: this.props.fields.split('|'), sort: "id", sortDir: "asc" } 
 	},
-	render: function(){		
-		var sorted_rows = this.props.rows.slice(0).sort( util.sorter( this.state.sort, this.state.sortDir ) );		
-		var rows = sorted_rows.map( function(r){
+	render: function(){		    
+		var sorted_rows = this.props.rows.slice(0).sort( util.sorter( this.state.sort, this.state.sortDir ) );				
+		var rows = sorted_rows.filter( util.search_object( this.props.search.split() ) ).map( function(r){
 			var cells = this.state.fields.map( function(k){
 				return <td className={k} key={r.id + "_" + k}>{r[k]}</td>;
 			});			
@@ -42,22 +42,29 @@ var TableAdmin = React.createClass({
 			showFields: this.props.showFields || this.props.fields, 
 			createFields: this.props.createFields || this.props.fields, 
 			updateFields: this.props.updateFields || this.props.fields,
+			search: "",
 			icon: this.props.icon || "table",
 			title: this.props.title || "Data"
 		} 
 	},	
 	componentDidMount: function() {
 		api.get( this.props.url, this, 'rows' );
+		this.refs.searchInput.getDOMNode().focus();
   	},	
-	render: function(){ 				
+  	setSearch: function(){
+  		console.log( "search: " + this.refs.searchInput.getDOMNode().value);
+  		this.setState({search: this.refs.searchInput.getDOMNode().value || ""});
+  	},
+	render: function(){ 
+	    console.log( this.state.search )				
 		return (
 			<div className="tableAdmin">
 			  <div className="tableHeader">
 			    <i className={"fa fa-" + this.state.icon}></i>
 			    <label>{this.state.title}</label>
-			    <input type='search' placeholder="Search..."/>
+			    <input ref="searchInput" onInput={this.setSearch} onChange={this.setSearch} type='search' placeholder="Search..." value={this.state.search}/>
 			   </div>
-			   <TableGrid fields={this.state.gridFields} rows={this.state.rows} />
+			   <TableGrid fields={this.state.gridFields} rows={this.state.rows} search={this.state.search}/>
 			</div>
 		);
 	}
