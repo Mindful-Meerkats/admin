249bed86e5768b594e0fe70d5fb7c7f027bffb78
Ralf Nieuwenhuijsen <r.nieuwenhuijsen@synovite.nl>
2015-04-27 17:20:49 +0200

    * removed key property from fieldset

--- public/jsx/table_grid.jsx
+++ public/jsx/table_grid.jsx
@@ -117,7 +117,7 @@ var TableForm = React.createClass({
 			} else if( fieldType === 'password' ){
 				control = <TableFormEditorText key={fieldName} data={this.state.data[fieldName]} onInput={this.sendInput.bind(this,fieldName)} type='password'/>
 			}
-			return <fieldset key={(this.state.data.id||"_create") + "_" + fieldName}><legend>{fieldName.humanize()}</legend>{control}</fieldset>
+			return <fieldset><legend>{fieldName.humanize()}</legend>{control}</fieldset>
 		}, this);
 	},
 	render: function(){		    
